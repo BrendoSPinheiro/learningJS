f17860469e6660ba488b24d85d293dc19d00823b
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-04 14:50:31 -0300

    feat: tipos de dados

---
+++ 01-Fundamentos/03-TiposDeDados.js
@@ -0,0 +1,23 @@
+let nome = 'Brendo';
+
+console.log(typeof nome); // imprime: String
+
+let idade = 20;
+
+console.log(typeof idade); // imprime Number
+
+let estuda = true;
+
+console.log(typeof estuda); // imprime: Boolean
+
+let pi = 3.14151617;
+
+console.log(typeof pi); // imprime: Number
+
+let funcao = function(){}
+
+console.log(typeof funcao); // imprime: Function
+
+let nulo = null;
+
+console.log(typeof nulo); // imprime: Object
