49fe535e0a71019567d64e8cdd566d5528d204f3
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-04 14:54:57 -0300

    feat: tipagem fraca

---
+++ 01-Fundamentos/02-TipagemFraca.js
@@ -0,0 +1,15 @@
+/**
+ * As variáveis do javascript podem ter seus tipos modificados durante o código
+ */
+
+let informacaoPessoa = 'Brendo';
+
+console.log(typeof informacaoPessoa); // imprime: String
+
+informacaoPessoa = 20;
+
+console.log(typeof informacaoPessoa); // imprime: Number
+
+informacaoPessoa = true;
+
+console.log(typeof informacaoPessoa); // imprime: Boolean
