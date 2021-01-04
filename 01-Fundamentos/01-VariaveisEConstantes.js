dbb56f33351f6cc0a54f9c6cfa93293f86c77e3f
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-04 14:21:49 -0300

    Variaveis e constantes

---
+++ 01-Fundamentos/01-VariaveisEConstantes.js
@@ -0,0 +1,14 @@
+// var, let e const
+var numero1 = 3;
+let numero2 = 5;
+
+console.log(numero1, numero2);
+
+var numero1 = 20;
+numero2 = 40;
+
+console.log(numero1, numero2);
+
+const nome = 'Brendo';
+
+console.log(nome);
