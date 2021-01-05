db97e71565a36c7f38efc23f5fe329434cbd13f6
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-04 22:00:00 -0300

    feat: arrays

---
+++ 01-Fundamentos/07-Arrays.js
@@ -0,0 +1,18 @@
+const nomes = ['Brendo', 'Andrei', 'Maisa', 'Jose'];
+console.log(nomes); // imprime ['Brendo', 'Andrei', 'Maisa', 'Jose']
+console.log(nomes[0], nomes[3]); // imprime Brendo e Jose
+
+// atribuindo novos valores ao array
+nomes[4] = 'Maiara';
+console.log(nomes); // imprime [ 'Brendo', 'Andrei', 'Maisa', 'Jose', 'Maiara' ]
+
+// adiciona um valor na última posição do array
+nomes.push('Janderson');
+console.log(nomes); // imprime [ 'Brendo', 'Andrei', 'Maisa', 'Jose', 'Maiara', 'Janderson' ]
+
+// Mostra a quantidade de elementos no array
+console.log(nomes.length); // imprime 6
+
+// Remove o último valor do array e retorna o mesmo
+console.log(nomes.pop()); // imprime Janderson
+console.log(nomes); // imprime [ 'Brendo', 'Andrei', 'Maisa', 'Jose', 'Maiara' ]
