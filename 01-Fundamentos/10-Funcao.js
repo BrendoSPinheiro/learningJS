53f6bf7d5dbac11b364bf4b5a3813c3025745971
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-05 15:21:39 -0300

    feat: funçao

---
+++ 01-Fundamentos/10-Funcao.js
@@ -0,0 +1,9 @@
+// Função em quase tudo no js
+
+console.log(typeof Object); // imprime o tipo function
+
+class Objeto {}
+
+console.log(typeof Objeto); // imprime o tipo function
+
+// OBS: Praticamente tudo no javascript é uma função
