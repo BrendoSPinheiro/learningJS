19ad71428c4fcf7fa753fb3c3012eccbfdd5e027
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-06 23:37:30 -0300

    feat: update funçao

--- 01-Fundamentos/10-Funcao.js
+++ 01-Fundamentos/10-Funcao.js
@@ -7,3 +7,37 @@ class Objeto {}
 console.log(typeof Objeto); // imprime o tipo function
 
 // OBS: Praticamente tudo no javascript é uma função
+// -----------------------------------------------------------
+
+// Função sem retorno
+function imprimeNomeCompleto(nomeCompleto) {
+  console.log(nomeCompleto);
+}
+
+imprimeNomeCompleto('Brendo Souza Pinheiro'); // imprime Brendo Souza Pinheiro
+imprimeNomeCompleto(); // Funciona mesmo se não passar o parâmetro, mas loga undefined
+
+// Função com retorno
+function montaNomeCompleto(nome, sobrenome) {
+  return `${nome} ${sobrenome}`;
+}
+
+const nomeCompleto = montaNomeCompleto('Brendo', 'Souza');
+
+const nomeCompleto2 = montaNomeCompleto('Andrei');
+
+imprimeNomeCompleto(nomeCompleto); // imprime Brendo Souza
+imprimeNomeCompleto(nomeCompleto2); // imprime Andrei undefined pois não passei o segundo parâmetro na linha 27
+
+// Função com retorno e valor padrão no parâmetro
+function soma(num1 = 0, num2 = 0) {
+  return num1 + num2;
+}
+
+const soma1 = soma(5, 5); // retorna 10
+const soma2 = soma(5); // retorna 5
+const soma3 = soma(); // retorna 0
+
+console.log(soma1);
+console.log(soma2);
+console.log(soma3);
