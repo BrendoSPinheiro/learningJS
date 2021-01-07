0c11123c083f3adb3393c2d550c4c87d78c219e5
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-07 20:13:11 -0300

    feat: update funçao

--- 01-Fundamentos/10-Funcao.js
+++ 01-Fundamentos/10-Funcao.js
@@ -1,4 +1,5 @@
 // Função em quase tudo no js
+console.log('Funções em quase tudo no js \n');
 
 console.log(typeof Object); // imprime o tipo function
 
@@ -7,8 +8,8 @@ class Objeto {}
 console.log(typeof Objeto); // imprime o tipo function
 
 // OBS: Praticamente tudo no javascript é uma função
-// -----------------------------------------------------------
-
+console.log('-----------------------------------------------------------------------------');
+console.log('Funções com e sem retorno \n');
 // Função sem retorno
 function imprimeNomeCompleto(nomeCompleto) {
   console.log(nomeCompleto);
@@ -41,3 +42,24 @@ const soma3 = soma(); // retorna 0
 console.log(soma1);
 console.log(soma2);
 console.log(soma3);
+
+console.log('-----------------------------------------------------------------------------');
+console.log('Funções em variáveis \n');
+// usando função anônima
+const imprimirSoma = function(num1, num2) {
+  console.log(num1 + num2);
+};
+
+imprimirSoma(5, 10);
+
+// usando arrow function
+const imprimeNome = (nome, sobrenome) => {
+  console.log(`${nome} ${sobrenome}`);
+};
+
+imprimeNome('Brendo', 'Souza');
+
+// usando arrow function com retorno implícito
+const subtração = (num1, num2) => num1 - num2;
+
+console.log(subtração(10, 5));
