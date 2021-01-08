cdc729de85cd899a1d28c5b9b49b34c3a02aa998
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-07 22:10:47 -0300

    feat: usando var e seu escopo

---
+++ 01-Fundamentos/11-UsandoVar.js
@@ -0,0 +1,30 @@
+// escopo: até onde algo está visível/disponível?
+
+/**
+ * Var:
+ *  - 2 escopos: global e função 
+ *  - visibilidade: dentro de um bloco que não seja função, variáveis declaradas com var são visíveis a todos
+ *                  dentro de função é apenas visível dentro da função
+ */
+
+// Var no escopo global
+
+{
+  {
+    {
+      var teste = 'teste escopo global';
+    }
+  }
+}
+
+console.log(teste); // imprime "teste" pois o var quando declarado fora de uma função é global.
+
+// Var no escopo de função
+
+function teste2() {
+  var texto = 'testando escopo de função';
+  console.log(texto);
+}
+
+teste2();
+// console.log(texto); // da erro pois var em escopo de função só é visível dentro da função
