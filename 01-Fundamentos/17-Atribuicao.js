89339fc21b8140ae0b427b11773fc3dea870c1c0
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-13 13:47:51 -0300

    feat: atribuiçao

---
+++ 01-Fundamentos/17-Atribuicao.js
@@ -0,0 +1,18 @@
+const a = 5; // atribuição
+let b = 5;
+
+b += a; // atribuição aditiva (b = b + a)
+console.log(b); // imprime 10
+
+b -= 4; // atribuição subtrativa (b = b - 4)
+console.log(b); // imprime 6
+
+b *= 2 // atribuição multiplicativa (b = b * 2)
+console.log(b); // imprime 12
+
+b /= 2; // atribuição divisiva (b = b / 2)
+console.log(b); // imprime 6
+
+b %= 2; // atribuição modular (b = b % 2)
+console.log(b); // imprime 0
+
