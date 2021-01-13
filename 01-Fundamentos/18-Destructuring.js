44c21ea99e9c98da7a59d9bc430efa7ffcfb911f
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-13 20:24:36 -0300

    feat: destructuring

---
+++ 01-Fundamentos/18-Destructuring.js
@@ -0,0 +1,33 @@
+const pessoa = {
+  nome: 'Brendo',
+  idade: 20,
+  endereco: {
+    logradouro: 'Rua carlos',
+    numero: 61,
+    casas: [
+      1,
+      2,
+      3,
+    ]
+  }
+}
+
+const { nome, idade } = pessoa;
+console.log(nome, idade);
+
+const { nome: name, idade: age } = pessoa; // mudando o nome da constante/variável
+console.log(name, age);
+
+/*
+Desestruturando objetos que estão dentro de outros objetos
+
+const { endereco: { logradouro, numero, casas: [ casa1, casa2, casa3 ] } } = pessoa;
+console.log(logradouro, numero); 
+
+ou
+
+const { endereco } = pessoa;
+const { logradouro, numero, casas } = endereco;
+const [ casa1, casa2, casa3 ] = casas;
+console.log(endereco, logradouro, numero, casas, casa1, casa2, casa3);
+*/
