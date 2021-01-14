b63ea2358811da8d6e749d73bbf900b48d35eba2
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-14 10:21:11 -0300

    feat Operadores Unarios

---
+++ 01-Fundamentos/22-OperadoresUnarios.js
@@ -0,0 +1,14 @@
+let num1 = 1;
+let num2 = 10;
+
+num1++; // incremento pós fixado
+console.log(num1); // imprime 2
+
+++num1; // incremento pré fixado
+console.log(num1);
+
+num2--; // decremento pós fixado
+console.log(num2); // imprime 9
+
+--num2; // decremento pré fixado
+console.log(num2); // imprime 8
