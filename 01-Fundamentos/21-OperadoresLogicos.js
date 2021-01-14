fc8a81b91715bd738da8805147c67abe5a6eda62
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-14 02:05:56 -0300

    feat Operadores logicos

---
+++ 01-Fundamentos/21-OperadoresLogicos.js
@@ -0,0 +1,16 @@
+function compras(trabalho1, trabalho2) {
+  const comprarSorvete = trabalho1 || trabalho2; // ou lógico
+  const comprarTv50 = trabalho1 && trabalho2; // e lógico
+  // const comprarTv32 = !!(trabalho1 ^ trabalho2) // bitwise xor (OU exclusivo)
+  const comprarTv32 = trabalho1 != trabalho2 // ou exclusivo pode ser simulado pela diferença
+  const manterSaudavel = !comprarSorvete; // negação lógica
+
+  return {
+    comprarSorvete,
+    comprarTv50,
+    comprarTv32,
+    manterSaudavel,
+  }
+}
+
+console.log(compras(false, false));
