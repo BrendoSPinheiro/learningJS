756da287f131403daa6293b2673971d76acc2031
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-01-13 21:29:34 -0300

    feat: operadores aritmeticos

---
+++ 01-Fundamentos/19-OperadoresAritmeticos.js
@@ -0,0 +1,16 @@
+const [ a, b, c, d ] = [20, 10, 5, 30];
+
+// Operador binário
+
+const soma = a + b + c + d;
+const subtracao = a - b;
+const multiplicacao = b * c;
+const divisao = d / b;
+const modulo = a % 2; // resto da divisão
+
+console.log(`Soma = ${soma}, 
+Subtração = ${subtracao}, 
+Multiplicação = ${multiplicacao},
+Divisão = ${divisao},
+Módulo = ${modulo}
+`);
